feeebb1dd7a28e80041a44531c2448a29a804f92
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-12-23 12:39:13 +0800

    Add results from Enrichr and GREAT

---
+++ protocolFunctions/installPackagesBenchmark.R
@@ -0,0 +1,18 @@
+installPackagesBenchmark <- function(){
+  
+  ## Installing packages for GSA tools
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager::install('seq2pathway')
+  BiocManager::install('broadenrich')
+  BiocManager::install('chipenrich')
+  
+  ## Loading Packages
+  
+  library(GSAChIPSeqGold)
+  library(seq2pathway)
+  library(chipenrich)
+  
+  ## For making maximum memory available to R session. 
+  memory.size(max = TRUE)
+}
