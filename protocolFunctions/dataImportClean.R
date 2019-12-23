ba06cb75bf40888d9fa3cf34f5d7c1cad5b052b0
Shaurya Jauhari <ishauryajauhari@gmail.com>
2019-12-23 15:14:26 +0800

    Relocate files

---
+++ protocolFunctions/dataImportClean.R
@@ -0,0 +1,34 @@
+dataImportClean <- function(){
+  
+    ### Importing the master table ###
+    
+    setwd(".")
+    proj_set()
+    ChIPSeqDataMaster <- read.csv (file = "GSAChIPSeqBenchmarkDatasetProfile.txt", sep = '\t', header = TRUE, quote = "")
+    
+    ## The following code creates a list of samples for which we need to extract the BED files for.
+    
+    ChIPSeqSamples<-as.character(ChIPSeqDataMaster$GSM)
+    
+    ## Initializing list for storing BED files and the consecutive GRanges objects.
+    
+    samplesInBED = list()
+    
+    for(i in 1:length(ChIPSeqSamples))
+    {
+      samplesInBED[[i]] <- read.table(paste0("./testData/",paste0(eval(parse(text="ChIPSeqSamples[i]")),".bed")), sep = "\t", header = FALSE)
+      samplesInBED[[i]] <- samplesInBED[[i]][,1:3]
+      colnames(samplesInBED[[i]]) <- c("chrom", "start", "end")
+      samplesInBED[[i]] <- samplesInBED[[i]][order(samplesInBED[[i]]$chrom),]
+      samplesInBED[[i]] <- GRanges(seqnames = samplesInBED[[i]]$chrom, ranges = IRanges(samplesInBED[[i]]$`start`, samplesInBED[[i]]$`end`))
+      genome(samplesInBED[[i]]) <- "hg38"
+    }
+    
+    ## Saving BED files as GRanges objects ##
+    
+    samplesInBED <- GRangesList(samplesInBED)
+    names(samplesInBED) <- ChIPSeqSamples
+}
+
+
+
---
+++ protocolFunctions/executeChipenrichBroadenrichSeq2pathway.R
@@ -0,0 +1,33 @@
+executeChipenrichBroadenrichSeq2pathway <- function(){
+## Testing individual data in benchmark dataset with each GSA tool package
+## Also, since several of the tools don't acknowledge the mitochondrial DNA ("chrMT") entries have to be removed from the BED files.
+
+regenerated_samples <- list()
+seq2pathway_results <- list()
+
+for (i in 1:length(ChIPSeqSamples))
+{
+  regenerated_samples[[i]] <- read_bed(paste0("./regen/",paste0(eval(parse(text="ChIPSeqSamples[i]")),".bed")))
+  seq2pathway_results[[i]] <- seq2pathway_run(regenerated_samples[[i]])
+}
+saveRDS(seq2pathway_results, file = "./Results/seq2pathway/seq2pathway_results")
+rm(seq2pathway_results)
+
+
+chipenrich_results <- list()
+for (i in 1:length(ChIPSeqSamples))
+{
+  chipenrich_results[[i]] <- chipenrich_run(paste0("./regen/",paste0(eval(parse(text="ChIPSeqSamples[i]")),".bed")))
+}
+saveRDS(chipenrich_results, file = "./Results/chipenrich/chipenrich_results")
+rm(chipenrich_results)
+
+
+broadenrich_results <- list()
+for (i in 1:length(ChIPSeqSamples))
+{
+  broadenrich_results[[i]] <- broadenrich_run(paste0("./regen/",paste0(eval(parse(text="ChIPSeqSamples[i]")),".bed")))
+}
+saveRDS(broadenrich_results, file = "./Results/broadenrich/broadenrich_results")
+rm(broadenrich_results)
+}
