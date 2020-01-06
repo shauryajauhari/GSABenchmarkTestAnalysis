f2f6ce2aeeb8a24033d67c94a25609174329462a
Shaurya Jauhari <ishauryajauhari@gmail.com>
2020-01-06 14:03:32 +0800

    Rename function name

---
+++ protocolFunctions/extractingValuedResults.R
@@ -0,0 +1,45 @@
+## This function works on pruning the relevant entries from the Chipenrich, Broadenrich, and Seq2pathway tools' results. We are solely interested in
+## the enrichment terms and the corresponding value of statistical significance (p).
+
+extractingValuedResults <- function(){
+    
+## Broadenrich
+
+broadenrichResultsShredded <- list()
+broadenrichResults <- readRDS("./results/Broadenrich/broadenrichResults")
+for( i in 1:length(broadenrichResults))
+{
+  broadenrichResultsShredded[[i]] <- broadenrichResults[[i]]$results[,c(2,4)]
+  
+}
+names(broadenrichResultsShredded) <- as.character(ChIPSeqSamples)
+saveRDS(broadenrichResultsShredded, file = "./results/Broadenrich/broadenrichResultsShredded")
+rm(broadenrichResults)
+
+## Chipenrich
+
+chipenrichResultsShredded <- list()
+chipenrichResults <- readRDS("./results/Chipenrich/chipenrichResults")
+for( i in 1:length(chipenrich_results))
+{
+  chipenrichResultsShredded[[i]] <- chipenrichResults[[i]]$results[,c(2,4)]
+  
+}
+names(chipenrichResultsShredded) <- as.character(ChIPSeqSamples)
+saveRDS(chipenrichResultsShredded, file = "./results/Chipenrich/chipenrichResultsShredded")
+rm(chipenrichResults)
+
+
+## Pruning the irrelevant results from Seq2pathway
+
+seq2pathwayResultsShredded <- list()
+seq2pathwayResults <- readRDS("./results/Seq2pathway/seq2pathwayResults")
+
+for (i in 1:length(seq2pathwayResults))
+{
+  seq2pathwayResultsShredded[[i]] <- seq2pathwayResults[[i]]$gene2pathway_result.FET$GO_BP[,c(1,3)]  
+}
+names(seq2pathwayResultsShredded)<- as.character(ChIPSeqSamples)
+saveRDS(seq2pathwayResultsShredded, file = "./results/Seq2pathway/seq2pathwayResultsShredded")
+rm(seq2pathwayResults)
+}
