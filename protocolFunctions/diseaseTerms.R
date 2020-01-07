371128b53057d515f3c227f8dd05de9f31f3daf1
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-01-07 15:47:18 +0800

    Rename function

--- protocolFunctions/diseaseTerms.R
+++ protocolFunctions/diseaseTerms.R
@@ -1,8 +1,10 @@
 diseaseGOTerms <- function(){
-
+## GO Terms
+  
 ## Import the manually downloaded GO terms (as a text file) from geneontology.org associated with each of the four
 ## diseases as depicted in the benchmark dataset.
 
+  
 gastricCancerGOTerms <- read.table("gastricCancerGOTerms.txt", sep = "\t", header = FALSE)
 prostateCancerGOTerms <- read.table("prostateCancerGOTerms.txt", sep = "\t", header = FALSE)
 alzheimersDiseaseGOTerms <- read.table("alzheimersDiseaseGOTerms.txt", sep = "\t", header = FALSE)
@@ -37,12 +39,29 @@ for (i in 1: length(alzheimersDiseaseGOTerms$V1))
   alzheimersDiseaseGOTerms$V1[i] <- substr(alzheimersDiseaseGOTerms$V1[i],1,10)
 }
 
-## Saving objects
+## KEGG Terms
 
-saveRDS(gastricCancerGOTerms, file = "gastricCancerGOTerms")
-saveRDS(colorectalCancerGOTerms, file = "colorectalCancerGOTerms")
-saveRDS(prostateCancerGOTerms, file = "prostateCancerGOTerms")
-saveRDS(alzheimersDiseaseGOTerms, file = "alzheimersDiseaseGOTerms")
+## We define a list for KEGG terms for each disease here. 
+## The foremost ID is the KEGG term for the disease (self-explanatory variable) and the trailing ids represent it's
+## subpathways.
 
+alzheimersDiseaseKEGGTerms <- c("hsa05010", "hsa00190", "hsa04210", "hsa04020")
+gastricCancerKEGGTerms <- c("hsa05226", "hsa05120", "hsa04115", "hsa04110", "hsa04310", "hsa04151", "hsa04350", "hsa04520", "hsa04010")
+prostateCancerKEGGTerms <- c("hsa05215", "hsa04110", "hsa04210", "hsa04151", "hsa05202", "hsa04115", "hsa04010", "hsa04060", "hsa00140")
+colorectalCancerKEGGTerms <- c("hsa05210", "hsa04310", "hsa04110", "hsa04210", "hsa04115", "hsa04151", "hsa04010", "hsa04350", "hsa04012", "hsa04150")
+
+
+## Merging KEGG and GO terms.
+gastricCancerPool <- c(gastricCancerGOTerms[[1]], gastricCancerKEGGTerms)
+colorectalCancerPool <- c(colorectalCancerGOTerms[[1]], colorectalCancerKEGGTerms)
+alzheimersDiseasePool <- c(alzheimersDiseaseGOTerms[[1]], alzheimersDiseaseKEGGTerms)
+prostateCancerPool <- c(prostateCancerGOTerms[[1]], prostateCancerKEGGTerms)
+
+
+# Saving these objects
+saveRDS(alzheimersDiseasePool,"alzheimersDiseasePool")
+saveRDS(prostateCancerPool, "prostateCancerPool")
+saveRDS(colorectalCancerPool, "colorectalCancerPool")
+saveRDS(gastricCancerPool, "gastricCancerPool")
 
 }
