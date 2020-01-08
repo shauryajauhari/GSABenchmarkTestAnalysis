ab4c926fafa36d755eed3792a674bfa5b3f27159
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-01-08 13:38:58 +0800

    Add minor update

--- protocolFunctions/calculatePrioritization.R
+++ protocolFunctions/calculatePrioritization.R
@@ -1,7 +1,6 @@
-toolsResults <- c("chipenrichResultsShredded", "broadenrichResultsShredded","seq2pathwayResultsShredded","enrichrResultsShredded","greatResultsShredded")
-diseasePools <- c("colorectalCancerPool", "alzheimersDiseasePool", "gastricCancerPool", "prostateCancerPool")
+## This function takes a number (tool index) as input and returns the prioritization data against each given disease.
 
-calculatePrioritization <- function(){
+calculatePrioritization <- function(tool){
   
   forPrioritization <- vector("list", length(disease_pools)) # output values to be filled in this list
   
@@ -12,11 +11,11 @@ calculatePrioritization <- function(){
       ranks <- list()
       x <- 1
       
-      for (i in 1:length(eval(parse(text=(paste0(paste0(tools_results[1],"$"), ChIPSeqSamples[sam]))))[[1]]))
+      for (i in 1:length(eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[[1]]))
       {
         for (j in (eval(parse(text=disease_pools[[d]]))))
         {
-          if((eval(parse(text=(paste0(paste0(tools_results[1],"$"), ChIPSeqSamples[sam]))))[[1]])[[i]] == j)
+          if((eval(parse(text=(paste0(paste0(tools_results[tool],"$"), ChIPSeqSamples[sam]))))[[1]])[[i]] == j)
           {
             ranks[[x]]<- i
             x <- x+1
@@ -24,17 +23,17 @@ calculatePrioritization <- function(){
         }
       }
       
-      for_prioritization <- as.double((ranks[[1]]/nrow((eval(parse(text=(paste0(paste0(tools_results[1],"$"), ChIPSeqSamples[sam]))))[1]))*100))
-      forPrioritization[[d]][[sam]] <- for_prioritization
+      findPrioritization <- as.double((ranks[[1]]/nrow((eval(parse(text=(paste0(paste0(tools_results[tool],"$"), 
+      ChIPSeqSamples[sam]))))[1]))*100))
+      forPrioritization[[d]][[sam]] <- findPrioritization
     }
   }
   
-  forPrioritization <- as.data.frame(forPrioritization)
-  
-  # Save object
-  saveRDS(forPrioritization,"forPrioritization")
+  forPrioritization <- as.data.frame(forPrioritization) # transform to a dataframe
+  colnames(forPrioritization) <- diseasePools
+  forPrioritization$Samples <- ChIPSeqSamples # add key attribute of sample names. this may be helpful for the purpose of joining dataframes.
+  return(forPrioritization)
 }
 
-## (For GREAT) Note that in the resultant table, the zero entries reflect that there are no matches between the 
-## disease terms and the tool output. While the empty cells mean that the output for the particular sample was not
-## available.
+## (For GREAT) Note that in the resultant table, the zero entries reflect that there are no matches between the disease terms and the tool 
+## output. While the empty cells mean that the output for the particular sample was not available.
