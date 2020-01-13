167f279634d76a75274d5a52d50e6dbfe2cd04b7
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-01-13 11:32:21 +0800

    Detach code for annotating final result

--- protocolFunctions/calculatePrioritization.R
+++ protocolFunctions/calculatePrioritization.R
@@ -33,20 +33,7 @@ calculatePrioritization <- function(tool){
   ## lists(results) may have different length components. To ensure fullness of the dataframe we shall calculate the maximum length amongst
   ## all (that should technically be the number of diseases), and structure the shorter lists to fit the dimensions of the dataframe by 
   ## explicit zeros. 
-tempList <- list()
 
-for(ind in 1: length(forPrioritization)){tempList <- append(tempList, length(forPrioritization[[ind]]))}
-maxNum <- max(unlist(tempList))
-forPrioritization <- lapply(forPrioritization, function(x) x[1:maxNum])
-
-
-forPrioritization <- as.data.frame(forPrioritization) # transform to a dataframe
-forPrioritization <- as.data.frame(t(forPrioritization)) # transpose the data frame as the output from the function is a list
-row.names(forPrioritization) <- NULL
-colnames(forPrioritization) <- diseasePools
-forPrioritization$Median <- apply(forPrioritization, 1, median) # median value shall be the basis of plotting the results.
-forPrioritization$Samples <- ChIPSeqSamples # add key attribute of sample names. this may be helpful for the purpose of joining dataframes.
-forPrioritization[is.na(forPrioritization)] <- 0 ## replacing NAs with zero for lists shorther than the maximum length.
 return(forPrioritization)
 }
 
