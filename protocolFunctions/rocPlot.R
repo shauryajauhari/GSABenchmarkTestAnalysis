6b0ccbba7c15c554a098ebaaa72c84b1a3c9acde
Shaurya Jauhari <shauryajauhari@gzhmu.edu.cn>
2020-01-14 17:26:02 +0800

    Rename functions

---
+++ protocolFunctions/rocPlot.R
@@ -0,0 +1,17 @@
+## The following function takes the sensitivity and specificity values for the respective tools and draws out an ROC.
+
+rocPlot <- function()
+{
+  plot(sort(1-plotSpecificity$chipenrichResultsShredded), sort(plotSensitivity$chipenrichResultsShredded), type="b", col=c("red"), 
+       xlab="1-Specificity",
+       ylab="Sensitivity", main = "Tools for Enrichment of Genomic Regions")
+  
+  lines(sort(1-plotSpecificity$broadenrichResultsShredded), sort(plotSensitivity$broadenrichResultsShredded), type="b", col=c("green"))
+  lines(1-plotSpecificity[order(plotSpecificity$enrichrResultsShredded),][,4], plotSensitivity[order(plotSensitivity$enrichrResultsShredded),][,4], type="b", col=c("blue"))
+  lines(1-plotSpecificity[order(plotSpecificity$seq2pathwayResultsShredded),][,3], plotSensitivity[order(plotSensitivity$seq2pathwayResultsShredded),][,3], type="b", col=c("magenta"))
+  lines(1-plotSpecificity[order(plotSpecificity$greatResultsShredded),][,5], plotSensitivity[order(plotSensitivity$greatResultsShredded),][,5], type="b", col=c("lightpink"))
+  
+  legend(-95, 70, legend=c("CHIPENRICH", "BROADENRICH", "ENRICHR", "SEQ2PATHWAY", "GREAT"),
+         col=c("red", "green", "blue", "magenta", "lightpink"), lty=1, cex=0.8, box.lty=0)
+}
+
--- protocolFunctions/simulationFalseNegatives.R
+++ protocolFunctions/simulationFalseNegatives.R
@@ -1,41 +1,42 @@
 # Generating 5 tracks from each parent/sample track from the benchmark dataset by deleting certain percentages of the total strength.}
 # reading sample data from a local folder | BED file and storing as a list
 
-Simulation_For_False_Negatives <- function(){
+simulationFalseNegatives <- function(){
 
-all_samples <- list()
+allSamples <- vector("list", length(ChIPSeqSamples))
+names(allSamples) <- ChIPSeqSamples
 for (i in 1:length(ChIPSeqSamples))
 {
-  all_samples[[i]] <- read.table(paste("./regen/",paste(eval(parse(text="ChIPSeqSamples[i]")),".bed", sep = ""), sep = ""))
-  all_samples[[i]] <- all_samples[[i]][,1:3] # extracting just the chromosomal number , start index , and end index.
-  names(all_samples[[i]]) <- c("chrom","start","end")
+  allSamples[[i]] <- as.data.frame(samplesInBED[[i]])
+  allSamples[[i]] <- allSamples[[i]][,1:3] # extracting just the chromosomal number , start index , and end index.
+  names(allSamples[[i]]) <- c("chrom","start","end")
 }
 
 # Function to break down lengths of child tracks as distinct percentages of the parent track. 
 # We have assumed percentages less than 50 because higher order deletions could significantly hamper the outputs. 
 # So, proportions of 10%, 20%, 30%, 40%, 50% are best suited.
 
-length_breakups <- list()
-track_lengths <- function(x){
-  listing_lengths <- list()
+lengthBreakups <- list()
+trackLengths <- function(x){
+  listingLengths <- list()
   for(i in 1:5)
   {
-    listing_lengths[[i]]<- round(((10*i)*nrow(x))/100);
+    listingLengths[[i]]<- round(((10*i)*nrow(x))/100)
   }
-  return(listing_lengths)
+  return(listingLengths)
 }
 
-length_breakups <- lapply(all_samples,track_lengths) # a list of lists.
+lengthBreakups <- lapply(allSamples, trackLengths) # a list of lists.
 
 # Block to create random tracks of distinct orders.
 ## Creating 2 dimensional lists (list of lists)
-child_tracks <- lapply(1:length(length_breakups), function(y) list())
-simulated_all_samples <- lapply(1:length(length_breakups), function(y) list())
-for(i in 1:length(length_breakups)) # 106
+childTracks <- lapply(1:length(lengthBreakups), function(y) list())
+simulatedAllSamples <- lapply(1:length(lengthBreakups), function(y) list())
+for(i in 1:length(lengthBreakups)) # 10
   {
     for(j in 1:5)
       {
-        child_tracks[[i]][[j]] <-all_samples[[i]][sample(nrow(all_samples[[i]]),length_breakups[[i]][[j]]),]
+        childTracks[[i]][[j]] <- allSamples[[i]][sample(nrow(allSamples[[i]]),lengthBreakups[[i]][[j]]),]
         # In the above expression, the attribute replace is set to FALSE by default. 
         # If TRUE, it shall add back rows to the main dataframe thereby carrying a chance to be chosen again and hence resulting in redundancy. 
         # The following expression will create 5 entries against each entry of all_samples, i.e. 5 different tracks from every dataset track 
@@ -43,12 +44,10 @@ for(i in 1:length(length_breakups)) # 106
         # (that has already been called; if not we can do it here) and engenders the remaining rows in first-argument-dataframe that are exclusive to it, 
         # subtracting the rows from second-argument-dataframe. Any of the following does the trick.
         library(dplyr)
-        simulated_all_samples[[i]][[j]] <- anti_join(all_samples[[i]],child_tracks[[i]][[j]])
-    
-        # simulated_all_samples[[i]][[j]] <- all_samples[[i]][is.na(match(all_samples[[i]],child_tracks[[i]][[j]])),]
-        # simulated_all_samples[[i]][[j]] <- all_samples[[i]][!all_samples[[i]]%in%child_tracks[[i]][[j]],]
+        simulatedAllSamples[[i]][[j]] <- anti_join(allSamples[[i]],childTracks[[i]][[j]])
       }
-  }
+}
+return(simulatedAllSamples)
 }
 
 
